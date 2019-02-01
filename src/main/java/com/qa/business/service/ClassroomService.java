321cbd4862783e0152a8e181320186b373849bfd
Matthew Horwell <mattjhorwell@gmail.com>
2019-02-01 11:56:45 +0000

    Implemented Classroom business rules

--- src/main/java/com/qa/business/service/ClassroomService.java
+++ src/main/java/com/qa/business/service/ClassroomService.java
@@ -1,5 +1,20 @@
 package com.qa.business.service;
 
 public interface ClassroomService {
+	
+	String getAllClassrooms();
+	
+	String getClassroom(Long id);
+	
+	String createClassroom(String classroom);
+	
+	String deleteClassroom(Long id);
+	
+	String updateClassroom(Long id, String classroom);
+	
+	String addTrainee(Long id, Long traineeID);
+	
+	String removeTrainee(Long id, Long traineeID);
+	
 
 }
--- src/main/java/com/qa/persistence/domain/Classroom.java
+++ src/main/java/com/qa/persistence/domain/Classroom.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Classroom {
@@ -15,6 +16,7 @@ public class Classroom {
 	
 	private String trainer;
 	
+	@OneToMany
 	private Collection<Trainee> trainees;
 	
 	public Classroom() {
