19e6e174ea038a292f97acae6039f06c7816ec72
Matthew Horwell <mattjhorwell@gmail.com>
2019-02-01 10:31:06 +0000

    Created Classroom and Trainee entities

--- src/main/java/com/qa/persistence/domain/Classroom.java
+++ src/main/java/com/qa/persistence/domain/Classroom.java
@@ -1,5 +1,44 @@
 package com.qa.persistence.domain;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Classroom {
+	
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	private String trainer;
+	
+	private List<Trainee> traineeList;
+	
+	public Classroom() {
+	}
+	
+	public Classroom(String trainer, List<Trainee> traineeList) {
+		this.trainer = trainer;
+		this.traineeList = traineeList;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTrainer() {
+		return trainer;
+	}
+
+	public void setTrainer(String trainer) {
+		this.trainer = trainer;
+	}
 
 }
--- src/main/java/com/qa/persistence/domain/Trainee.java
+++ src/main/java/com/qa/persistence/domain/Trainee.java
@@ -1,5 +1,39 @@
 package com.qa.persistence.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Trainee {
+	
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	private String traineeName;
+	
+	public Trainee() {
+	}
+	
+	public Trainee(String traineeName) {
+		this.traineeName = traineeName;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTraineeName() {
+		return this.traineeName;
+	}
+
+	public void getTraineeName(String traineeName) {
+		this.traineeName = traineeName;
+	}
 
 }
