bba45d860919bdbe4d495384d4b2e6321009a2cb
Matthew Horwell <mattjhorwell@gmail.com>
2019-02-01 12:00:30 +0000

    Implemented trainee business methods.

--- src/main/java/com/qa/business/service/TraineeService.java
+++ src/main/java/com/qa/business/service/TraineeService.java
@@ -1,5 +1,15 @@
 package com.qa.business.service;
 
 public interface TraineeService {
+	
+	String getAllTrainees();
+	
+	String getTrainee(Long id);
+	
+	String createTrainee(String trainee);
+	
+	String deleteTrainee(Long id);
+	
+	String updateTrainee(Long id, String trainee);
 
 }
--- src/main/java/com/qa/business/service/TraineeServiceImpl.java
+++ src/main/java/com/qa/business/service/TraineeServiceImpl.java
@@ -1,5 +1,42 @@
 package com.qa.business.service;
 
-public class TraineeServiceImpl {
+import static javax.transaction.Transactional.TxType.REQUIRED;
+import static javax.transaction.Transactional.TxType.SUPPORTS;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+import com.qa.persistence.repository.TraineeRepository;
+
+@Transactional(SUPPORTS)
+public class TraineeServiceImpl implements TraineeService{
+	
+	@Inject
+	private TraineeRepository repo;
+	
+
+	public String getAllTrainees() {
+		return repo.getAllTrainees();
+	}
+
+	public String getTrainee(Long id) {
+		return repo.getTrainee(id);
+	}
+
+	@Transactional(REQUIRED)
+	public String createTrainee(String trainee) {
+		return repo.createTrainee(trainee);
+	}
+
+	@Transactional(REQUIRED)
+	public String deleteTrainee(Long id) {
+		return repo.deleteTrainee(id);
+	}
+
+	@Transactional(REQUIRED)
+	public String updateTrainee(Long id, String trainee) {
+		return repo.updateTrainee(id, trainee);
+	}
+	
 
 }
